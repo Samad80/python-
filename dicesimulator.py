57e74a16974ff1b7ff28de5b8e0e9e762b930654
Samad80 <101449155+Samad80@users.noreply.github.com>
2023-08-04 14:01:41 -0700

    Update dicesimulator.py

--- dicesimulator.py
+++ dicesimulator.py
@@ -4,7 +4,9 @@ def dice():
     return random.randint(1,6)
 
 def main():
-   for i in range (2):
+    print("press enter to roll a dice")
+    input ()
+    for i in range (2):
     print("you rolled a", dice())
 
 main()
