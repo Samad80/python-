4d58364144bffcf157e0bfe8e8a12cc38a9f594b
Samad80 <101449155+Samad80@users.noreply.github.com>
2023-08-08 14:19:01 -0700

    Create tempconverter.py

---
+++ tempconverter.py
@@ -0,0 +1,52 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def fahrenheit_to_kelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5/9
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+def kelvin_to_fahrenheit(kelvin):
+    return (kelvin * 9/5) - 459.67
+
+def main():
+    print("Temperature Converter Program")
+    print("1. Celsius to Fahrenheit")
+    print("2. Celsius to Kelvin")
+    print("3. Fahrenheit to Celsius")
+    print("4. Fahrenheit to Kelvin")
+    print("5. Kelvin to Celsius")
+    print("6. Kelvin to Fahrenheit")
+
+    choice = int(input("Enter your choice (1/2/3/4/5/6): "))
+    
+    if choice == 1:
+        celsius = float(input("Enter temperature in Celsius: "))
+        print("Temperature in Fahrenheit:", celsius_to_fahrenheit(celsius))
+    elif choice == 2:
+        celsius = float(input("Enter temperature in Celsius: "))
+        print("Temperature in Kelvin:", celsius_to_kelvin(celsius))
+    elif choice == 3:
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+        print("Temperature in Celsius:", fahrenheit_to_celsius(fahrenheit))
+    elif choice == 4:
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+        print("Temperature in Kelvin:", fahrenheit_to_kelvin(fahrenheit))
+    elif choice == 5:
+        kelvin = float(input("Enter temperature in Kelvin: "))
+        print("Temperature in Celsius:", kelvin_to_celsius(kelvin))
+    elif choice == 6:
+        kelvin = float(input("Enter temperature in Kelvin: "))
+        print("Temperature in Fahrenheit:", kelvin_to_fahrenheit(kelvin))
+    else:
+        print("Invalid choice. Please select a valid option.")
+
+if __name__ == "__main__":
+    main()
